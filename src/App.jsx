92bd02a37805014a7be95ae4d0637f53deb8e81f
ivancarrascov <iven91@gmail.com>
2025-08-31 20:19:38 -0400

    Hice ediciones de testeo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
 import "./style/App.css";
+import Counter from "./components/Contador.jsx";
+import Greeting from "./components/Saludo.jsx";
 
 function App() {
-  return <></>;
+
+  
+  
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>¡Hola desde React con Vite! 🎉</h1>
+      <p>Este es mi primer proyecto en React</p>
+
+      <Counter />
+      <Greeting />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Saludo.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <div>
+      <h2>¡Hola, React!</h2>
+      <p>Este es mi primer componente.</p>
+    </div>
+  );
+}
+
+export default Greeting;
