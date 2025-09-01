bf285606c2fd39131d1aa5524531b90f6f7f6f74
Antonio Chihuailaf <antonio.caddin@gmail.com>
2025-08-31 20:40:47 -0400

    [COL-2][Chiwa][Se agrega Productos y css]

---
+++ src/data/productos.js
@@ -0,0 +1,37 @@
+import manzana from "../assets/img/manzana_roja.jpg";
+import banana from "../assets/img/banana.jpg";
+import naranja from "../assets/img/naranja.jpg";
+import pomelo from "../assets/img/pomelo.jpg"
+import placeholder from "../assets/img/placeholder.jpg"
+const productos = [
+    { id: 1, categoria: "fruta", nombre: "Manzana Roja", imagen: manzana, precio: 1240,
+        marca: " Fuji Industry",
+        cantidad: "500g", 
+        info: "Precio x Kg: $" },
+    { id: 2, categoria: "fruta", nombre: "Banana", imagen: banana, precio: 1150,
+        marca: " Fuji Industry",
+        cantidad: "500g", 
+        info: "Precio x Kg: $"  },
+    { id: 3, categoria: "fruta", nombre: "Naranja", imagen: naranja, precio: 1350,
+        marca: " Fuji Industry",
+        cantidad: "500g", 
+        info: "Precio x Kg: $"  },
+    { id: 4, categoria: "fruta", nombre: "Pomelo", imagen: pomelo, precio: 1450,
+        marca: " Fuji Industry",
+        cantidad: "500g",
+        info: "Precio x Kg: $" },
+    { id: 5, categoria: "fruta", nombre: "PlaceHolder", imagen: placeholder, precio: 1234,
+        marca: " Fuji Industry",
+        cantidad: "500g",
+        info: "Precio x Kg: $" },
+    { id: 6, categoria: "fruta", nombre: "PlaceHolder", imagen: placeholder, precio: 1234,
+        marca: " Fuji Industry",
+        cantidad: "500g",
+        info: "Precio x Kg: $" },
+    { id: 7, categoria: "fruta", nombre: "PlaceHolder", imagen: placeholder, precio: 1234,
+    marca: " Fuji Industry",
+    cantidad: "500g",
+    info: "Precio x Kg: $" }
+];
+
+export default productos;
